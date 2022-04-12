98c61d708bf0b5203961b05264418ebb93b40eed
AidanUnhappy <aidanczhai@gmail.com>
2022-04-12 17:49:17 +0800

    zsh -

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,9 @@
 export AIDAN=$HOME/Aidan
 export AIDAN_CONFIG=$AIDAN/FD/config
-export https_proxy=http://127.0.0.1:8889
+export AIDAN_SCRIPT=$AIDAN/FD/script
+
+export HTTP_PROXY="http://127.0.0.1:8889"
+export HTTPS_PROXY="http://127.0.0.1:8889"
 
 export VISUAL=nvim
 export EDITOR=nvim
