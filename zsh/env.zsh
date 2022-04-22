441b2bb3ac79bda9f4d1a22ae19021cacefa739e
AidanUnhappy <aidanczhai@gmail.com>
2022-04-22 12:47:15 +0800

    zsh adjust http_proxy

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,8 +2,10 @@ export AIDAN=$HOME/Aidan
 export AIDAN_CONFIG=$AIDAN/FD/config
 export AIDAN_SCRIPT=$AIDAN/FD/script
 
-export HTTP_PROXY="http://127.0.0.1:8889"
-export HTTPS_PROXY="http://127.0.0.1:8889"
+export http_proxy="http://127.0.0.1:8889/"
+export HTTP_PROXY=$http_proxy
+export https_proxy=$http_proxy
+export HTTPS_PROXY=$http_proxy
 
 export VISUAL=nvim
 export EDITOR=nvim
