7e6d961b376ea57c19bf2e5454d948f5b189d8a2
AidanUnhappy <aidanczhai@gmail.com>
2022-05-01 20:01:56 +0800

    zsh adjust ls

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,4 +3,13 @@ alias q="exit"
 
 alias ra="ranger"
 alias now="nvim $AIDAN_AC/Vw/index.md"
-alias gccs='git config credential.helper store'
+alias gccs="git config credential.helper store"
+
+alias ls="ls -A -v --group-directories-first --color=auto"
+alias ll="ls -l"
+
+alias chmod="chmod --preserve-root -v"
+alias chown="chown --preserve-root -v"
+alias df="df -h"
+alias du="du -h"
+alias grep="grep --color=auto"
