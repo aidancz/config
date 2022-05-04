bd822389430bf91cb2ddea0dded8e06ca2bf6cd4
AidanUnhappy <aidanczhai@gmail.com>
2022-05-04 11:54:06 +0800

    zsh above the sequence

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,7 +1,13 @@
 export AIDAN=$HOME/Aidan
 export AIDAN_AC=$AIDAN/_/AC
+export AIDAN_DK=$AIDAN/_/DK
 export AIDAN_ED=$AIDAN/_/ED
+export AIDAN_KL=$AIDAN/_/KL
+export AIDAN_LM=$AIDAN/_/LM
+export AIDAN_SD=$AIDAN/_/SD
 export AIDAN_FD=$AIDAN/FD
+export AIDAN_GA=$AIDAN/GA
+export AIDAN_GO=$AIDAN/GO
 
 export http_proxy="http://127.0.0.1:8889/"
 export HTTP_PROXY=$http_proxy
