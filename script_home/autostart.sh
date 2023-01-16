bbbf67d5032f914d0857ced9da6054ffdfa85bb0
aidancz <aidanczhai@gmail.com>
2023-01-16 12:10:55 +0800

    rename dir
    / -> /config_home
    /dev/null -> /script_home

---
+++ script_home/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export scr=/home/ai/a_gi/script
+/bin/bash $scr/dwm_status_loop.sh &
+/bin/bash $scr/keyboard.sh &
+/bin/bash $scr/screen.sh &
+
+#hsetroot
+#picom -b --no-fading-openclose
+xfce4-power-manager &
+nm-applet & #networkmanager
+
+sleep 2
+#qv2ray &
+nutstore &
